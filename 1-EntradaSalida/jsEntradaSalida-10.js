d7133f9c3b76708a070f1003e2d39e303ab048ff
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2018-07-06 21:58:29 -0300

    Ejercicios
    Ejercicios del 6 al 10.

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldo;
+	var descuento;
+	var sueldoConDescuento;
+
+	sueldo = document.getElementById('importe').value;
+	sueldo = parseInt(sueldo);
+	descuento = sueldo * 25 / 100;
+	sueldoConDescuento = sueldo - descuento;
+	document.getElementById('resultado').value = sueldoConDescuento;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var aumento;
+	var sueldoAumentado;
 
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+	aumento = sueldo * 10 / 100;
+	sueldoAumentado = sueldo + aumento;
+	document.getElementById('resultado').value = sueldoAumentado;
 	
 }
