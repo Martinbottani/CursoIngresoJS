c61702aa142899cd0ede539e0e13c5cc3e0b498b
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2018-07-06 21:57:41 -0300

    Modelo de parcial
    ejercicios del 1 al 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base = prompt("Dame la base: ");
+	altura = prompt("Dame la altura: ");
+
+	base = parseInt(base);
+	altura = parseInt(altura);
+
+	superficie = base * altura / 2;
+	perimetro = base * 3;
+
+	alert("La superficie es: " + superficie + " y el perimetro es: " + perimetro);
 
 }
