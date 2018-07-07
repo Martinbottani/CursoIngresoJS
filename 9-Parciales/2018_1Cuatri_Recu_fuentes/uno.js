bb7249f9a124294a79ae33ef935dd51dff517be9
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2018-07-06 22:20:40 -0300

    Otro enunciado
    Enunciado dado por el profe

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,6 +1,7 @@
 
 function mostrar()
 {
+	/*
 	var base;
 	var altura;
 	var superficie;
@@ -16,5 +17,29 @@ function mostrar()
 	perimetro = base * 3;
 
 	alert("La superficie es: " + superficie + " y el perimetro es: " + perimetro);
+
+	 Enunciado nuevo: se ingresan 3 numeros, se muestra:
+	1 la suma de los tres
+	2 el promedio de los 3
+	3 la resta de los 3
+	con un alert */
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var suma;
+	var promedio;
+	var resta;
+
+	numeroUno = prompt("Dame la nota 1: ");
+	numeroDos = prompt("Dame la nota 2:");
+	numeroTres = prompt("Dame la notra 3:");
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+	numeroTres = parseInt(numeroTres);
+	suma = numeroUno + numeroDos + numeroTres;
+	promedio = (numeroUno + numeroDos + numeroTres) / 3;
+	resta = numeroUno - numeroDos - numeroTres;
+
+	alert("la suma es: " + suma + ", el promedio es: " + promedio + " y la resta es: " + resta);
 
 }
