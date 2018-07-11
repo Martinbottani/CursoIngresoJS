2f87bd8ecf3bbdfa3534f59d672c50b1cc7ed105
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2018-07-10 22:14:59 -0300

    Ejercicios de Parcial
    Ejercicios de Parcial del 1 a 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var precioFinal;
+
+	precio = prompt("Dame el precio ");
+	porcentaje = prompt("Dame el porcentaje ");
+	precio = parseInt(precio);
+	porcentaje= parseInt(porcentaje);
+	descuento = precio * porcentaje /100;
+	precioFinal = precio - descuento;
+
+	document.getElementById('elPrecioFinal').value = precioFinal;
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
 
+	ancho = prompt("ingrese el ancho: ");
+	largo = prompt("ingrese el largo: ");
+	ancho = parseInt(ancho);
+	largo = parseInt(largo);
+
+	perimetro = (largo + ancho) *2;
+	alert("El perimetro es " + perimetro);
 }
