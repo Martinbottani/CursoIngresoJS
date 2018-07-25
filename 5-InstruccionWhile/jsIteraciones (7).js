5425da93726489ab9a7c16120d9a625453df53f3
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2018-07-24 22:08:32 -0300

    Ejercicios del 1 al 9 WHILE
    Ejercicios hechos, queda solo el 10

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,9 +1,17 @@
 function mostrar()
 {
-
+	var numeros;
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	while(respuesta == 'si')
+	{
+		numeros = prompt("Dame un número:");
+		numeros = parseInt(numeros);
+		acumulador += numeros;
+		contador++;
+		respuesta = prompt("Desea ingresar otro número?");
+	}
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,43 @@
 function mostrar()
 {
-
+	var numeroIngresado;
+	/*
+	var maximo = -999999;
+	var minimo = 999999;
+	*/
 	var contador=0;
-	// declarar variables
-	
 	var respuesta='si';
+	var bandera;
+	
+
+	bandera = 0;
 
 	while(respuesta!='no')
 	{
-		
-	
+		contador++;
+		numeroIngresado = prompt("Dame el número #" + contador + ":");
+		numeroIngresado = parseInt(numeroIngresado);
+		if(bandera == 0) 
+		{
+			maximo = numeroIngresado;
+			minimo = numeroIngresado;
+			bandera = 1;
+		}
+		else
+		{
+			if(numeroIngresado > maximo) 
+			{
+				maximo = numeroIngresado;
+			}
+			if(numeroIngresado < minimo) 
+			{
+				minimo = numeroIngresado;
+			}
+		}
+		respuesta = prompt("ingrese no, para salir");
 	}
-
-
-
+	
+	document.getElementById('maximo').value = maximo;
+	document.getElementById('minimo').value = minimo;
 
 }//FIN DE LA FUNCIÓN
